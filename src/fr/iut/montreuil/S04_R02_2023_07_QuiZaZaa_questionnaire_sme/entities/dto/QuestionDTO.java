9ae9aba7770dd708aa349a2b92caae566688cefa
Lony027 <iony@protonmail.com>
2023-02-16 14:38:02 +0100

    ajout getter/setters

--- src/fr/iut/montreuil/S04_R02_2023_07_QuiZaZaa_questionnaire_sme/entities/dto/QuestionDTO.java
+++ src/fr/iut/montreuil/S04_R02_2023_07_QuiZaZaa_questionnaire_sme/entities/dto/QuestionDTO.java
@@ -6,10 +6,65 @@ public class QuestionDTO {
 	private int nbBonneReponses;
 	private int nbMauvaisesReponses;
 	
-//	private int idQuestionnaire;
-//	private String langue;
-//	private String question;
-//	private String reponse;
-//	private String source;
-//	private int difficulte;
+	private int idQuestionnaire;
+	private String langue;
+	private String question;
+	private String reponse;
+	private String source;
+	private int difficulte;
+	
+	public int getIdQuestion() {
+		return idQuestion;
+	}
+	public void setIdQuestion(int idQuestion) {
+		this.idQuestion = idQuestion;
+	}
+	public int getNbBonneReponses() {
+		return nbBonneReponses;
+	}
+	public void setNbBonneReponses(int nbBonneReponses) {
+		this.nbBonneReponses = nbBonneReponses;
+	}
+	public int getNbMauvaisesReponses() {
+		return nbMauvaisesReponses;
+	}
+	public void setNbMauvaisesReponses(int nbMauvaisesReponses) {
+		this.nbMauvaisesReponses = nbMauvaisesReponses;
+	}
+	public int getIdQuestionnaire() {
+		return idQuestionnaire;
+	}
+	public void setIdQuestionnaire(int idQuestionnaire) {
+		this.idQuestionnaire = idQuestionnaire;
+	}
+	public String getLangue() {
+		return langue;
+	}
+	public void setLangue(String langue) {
+		this.langue = langue;
+	}
+	public String getQuestion() {
+		return question;
+	}
+	public void setQuestion(String question) {
+		this.question = question;
+	}
+	public String getReponse() {
+		return reponse;
+	}
+	public void setReponse(String reponse) {
+		this.reponse = reponse;
+	}
+	public String getSource() {
+		return source;
+	}
+	public void setSource(String source) {
+		this.source = source;
+	}
+	public int getDifficulte() {
+		return difficulte;
+	}
+	public void setDifficulte(int difficulte) {
+		this.difficulte = difficulte;
+	}
 }
